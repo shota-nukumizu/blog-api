7cdcbab3187e55a1913f57baf2f107ac00cfa5ce
shota-nukumizu <author2000.1225@gmail.com>
2022-01-14 08:27:35 +0900

    implement admin-site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from markdownx.admin import MarkdownxModelAdmin
+from .models import ArticleModel, TagModel
 
-# Register your models here.
+class ArticleAdmin(MarkdownxModelAdmin):
+    list_display = (
+        'title',
+        'get_tag',
+        'slug',
+        'is_public',
+        'created_at'
+    )
+    list_display_links = list_display
+    ordering = ('-created_at', )
+
+    def get_tag(self, row):
+        return ','.join([x.name for x in row.tag.all()])
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color')
+
+admin.site.register(ArticleModel, ArticleAdmin)
+admin.site.register(TagModel, TagAdmin)
