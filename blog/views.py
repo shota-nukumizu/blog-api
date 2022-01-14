56028764ff8aae33fd4b6825782c6e12920f30c6
shota-nukumizu <author2000.1225@gmail.com>
2022-01-14 09:04:49 +0900

    add permissions

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,17 @@
 from rest_framework import viewsets
 
 from .models import TagModel, ArticleModel
+from .permissions import IsAdminOrReadOnly
 from .serializers import ArticleSerializer, TagSerializer
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = TagModel.objects.all()
     serializer_class = TagSerializer
+    permission_classes = (IsAdminOrReadOnly, )
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = ArticleModel.objects.filter(is_public=True)
     serializer_class = ArticleSerializer
+    permission_classes = (IsAdminOrReadOnly, )
     lookup_field = 'slug'
