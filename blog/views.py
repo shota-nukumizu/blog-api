412e6c55d6e808f357b5e6134e83f38b5afbb3ac
shota-nukumizu <author2000.1225@gmail.com>
2022-01-14 09:16:35 +0900

    add pagination

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from .models import TagModel, ArticleModel
 from .permissions import IsAdminOrReadOnly
 from .serializers import ArticleSerializer, TagSerializer
+from .paginations import CustomPagination
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = TagModel.objects.all()
@@ -14,4 +15,5 @@ class ArticleViewSet(viewsets.ModelViewSet):
     queryset = ArticleModel.objects.filter(is_public=True)
     serializer_class = ArticleSerializer
     permission_classes = (IsAdminOrReadOnly, )
+    pagination_class = CustomPagination
     lookup_field = 'slug'
